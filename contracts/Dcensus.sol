1e98358af4e2c56169359e50b57b30425013f489
Ahmad Wilson <ahmadwilson@admins-MacBook-Pro.local>
2018-08-28 12:00:07 -0700

    Fix event emit and ownable constructor syntax compilation warnings

--- contracts/Dcensus.sol
+++ contracts/Dcensus.sol
@@ -28,20 +28,19 @@ contract DCensus {
     function addStat(string _category, string _value) public {
         statsData[_category].push(Statistic(msg.sender, _category, _value));
         
-	LogAddStat(msg.sender, _category, _value);
+	emit LogAddStat(msg.sender, _category, _value);
     }
 
     function getStat(string _category) public view returns (string) {
 	for (uint i = 0; i < statsData[_category].length; i++) {
 		if (statsData[_category][i].owner == msg.sender) {
-			string val = statsData[_category][i].value;
-			LogGetStat(msg.sender, _category, val);
+			emit LogGetStat(msg.sender, _category, statsData[_category][i].value);
 			
-			return val;
+			return statsData[_category][i].value;
 		}
 	}
 
-	LogGetStat(msg.sender, _category, 'Unknown');
+	emit LogGetStat(msg.sender, _category, 'Unknown');
 	return "Unable to find ${_category} statistic";
     }
 }
